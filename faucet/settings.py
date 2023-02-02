b5c5f271392c969e8381818c4b5f3d0c1f7405c5
Murray Groves <github@murraygrov.es>
2023-02-02 20:08:39 +0000

    Fix CSRF

--- faucet/settings.py
+++ faucet/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = open("private/django_secret").read().strip()
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production    !
 DEBUG = True
 
-ALLOWED_HOSTS = ["test.mydomain.com", "https://faucet.murraygrov.es"]
+ALLOWED_HOSTS = ["test.mydomain.com", "faucet.murraygrov.es"]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
